6308f19bfc1fbdec9c093840b661189454873d79
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-03 18:21:25 +0700

    functions

--- d.js
+++ d.js
@@ -24,7 +24,7 @@ function nhanPromise(a, b) {
 
 function chiaPromise(a, b) {
     return new Promise((resolve, reject) => {
-        const url = `http://localhost:3000/tinh/NHAN/${a}/${b}`;
+        const url = `http://localhost:3000/tinh/CHIA/${a}/${b}`;
         if (isNaN(a) || isNaN(b)) return reject(new Error('Type error.'));
         if (b == 0) return reject(new Error('Divide by zero.'));
         request(url, (error, response, body) => {
@@ -33,3 +33,23 @@ function chiaPromise(a, b) {
         });
     });
 }
+
+function tinhDienTichHinhThang(a, b, h, cb) {
+    return congPromise(a, b)
+    .then(tong => nhanPromise(tong, h))
+    .then(tich => chiaPromise(tich, 2));
+}
+
+tinhDienTichHinhThang(4, 5, 6)
+.then(result => console.log(result))
+.catch(error => console.log(error));
+
+// tinhDienTichHinhThang(4, 5, 'x', (error, result) => {
+//     console.log(error || result);
+// });
+
+// congPromise(4, 5)
+// .then(tong => nhanPromise(tong, 6))
+// .then(tich => chiaPromise(tich, 2))
+// .then(kq => console.log(kq))
+// .catch(error => console.log(error.message));
