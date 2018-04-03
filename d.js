fb9ff7faf398b01f926ca713913a97b43bf78e7a
Van Pho Nguyen <vanpho02@gmail.com>
2018-04-03 17:58:29 +0700

    cong nhan chia

--- d.js
+++ d.js
@@ -11,6 +11,25 @@ function congPromise(a, b) {
     });
 }
 
-congPromise('x', 5)
-.then(result => console.log(result))
-.catch(error => console.log(error.message));
+function nhanPromise(a, b) {
+    return new Promise((resolve, reject) => {
+        const url = `http://localhost:3000/tinh/NHAN/${a}/${b}`;
+        if (isNaN(a) || isNaN(b)) return reject(new Error('Type error.'));
+        request(url, (error, response, body) => {
+            if (error) return reject(error);
+            resolve(body);
+        });
+    });
+}
+
+function chiaPromise(a, b) {
+    return new Promise((resolve, reject) => {
+        const url = `http://localhost:3000/tinh/NHAN/${a}/${b}`;
+        if (isNaN(a) || isNaN(b)) return reject(new Error('Type error.'));
+        if (b == 0) return reject(new Error('Divide by zero.'));
+        request(url, (error, response, body) => {
+            if (error) return reject(error);
+            resolve(body);
+        });
+    });
+}
